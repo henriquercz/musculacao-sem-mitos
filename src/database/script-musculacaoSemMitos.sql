7efe721b81ac9a9f24b3d5892fd7d9298dad4714
henriquercz <henriquechagas06@gmail.com>
2026-05-09 00:07:56 -0300

    novas tabelas banco de dados

--- src/database/script-musculacaoSemMitos.sql
+++ src/database/script-musculacaoSemMitos.sql
@@ -6,8 +6,19 @@ idUser int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
 senha varchar(45),
+objetivo varchar(45),
+	constraint chkObjetivoUser check (objetivo in ('Ganhar Massa', 'Perder Peso', 'Recomposição Corporal', 'Saúde Geral')),
 conheceMetodologia char(3),
-constraint chk_conheceMetodologia check(conheceMetodologia in ('sim', 'nao'))
+	constraint chk_conheceMetodologia check(conheceMetodologia in ('sim', 'nao'))
+);
+
+create table registro_peso (
+idRegistro int primary key auto_increment,
+peso decimal(6,3),
+dtRegistro date,
+fkUsuario int,
+	constraint fkRegistroPesoUser foreign key (fkUsuario)
+		references usuario(idUser)
 );
 
 select * from usuario;
